bdc3a7c3f7f7d6750ddb91344086394a72d3538d
ivandangelo <ivanuntref@gmail.com>
2020-11-18 16:16:16 -0300

    interfaz inicio,registrar y login

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -16,8 +16,16 @@ var app = new Framework7({
     // Add default routes
     routes: [
       {
-        path: '/about/',
-        url: 'about.html',
+        path: '/me/',
+        url: 'me.html',
+      },
+      {
+        path: '/index/',
+        url: 'index.html',
+      },
+      {
+        path: '/regis_login_panel/',
+        url: 'regis_login_panel.html',
       },
     ]
     // ... other parameters
@@ -28,17 +36,29 @@ var mainView = app.views.create('.view-main');
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
+
 });
 
 // Option 1. Using one 'page:init' handler for all pages
+/*
 $$(document).on('page:init', function (e) {
     // Do something here when page loaded and initialized
     console.log(e);
 })
+*/
 
 // Option 2. Using live 'page:init' event handlers for each page
-$$(document).on('page:init', '.page[data-name="about"]', function (e) {
+$$(document).on('page:init', '.page[data-name="regis_login_panel"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
+    console.log('regis_login_panel.html');
+})
+
+$$(document).on('page:init', '.page[data-name="me"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log('me.html');
+})
+
+$$(document).on('page:init', '.page[data-name="index"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log('index.html');
 })
